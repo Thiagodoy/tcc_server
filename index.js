de336ca175cdafff357a87559abd800a89b372f9
Thiago H. Godoy <thiagodoy@hotmail.com>
2017-07-26 22:21:18 -0300

    Criado serviço UPLOAD

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-var server = express();
+const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
 
-server.get('/',(request,response)=>{
-    response.send('Servidor no Ar!!');
-});
+var server = express();
 
 
 const listUser = [
@@ -11,9 +11,47 @@ const listUser = [
     {nome:'Pedro', profissao:'Pintor'},
 ];
 
+server.get('/',(request,response)=>{
+    response.send('Servidor no Ar!!');
+});
+
 server.get('/api/getUsers',(request,response)=>{
     response.send(listUser);
 
 });
 
-server.listen(9000);
+server.post('/uploads',(req,response)=>{
+     // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+server.listen(9000,()=>{
+    console.log('Servidor no ar');
+
+});
+ 
