a858ac36e6286b236c3a67c42d8763f51a4f228b
Thiago H. Godoy <thiagodoy@hotmail.com>
2017-07-27 23:05:51 -0300

    Implmentação do servico para salvar logs

--- index.js
+++ index.js
@@ -1,57 +1,73 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const path = require('path');
 const formidable = require('formidable');
 const fs = require('fs');
 
+var sqlite3 = require('sqlite3').verbose();
+
+
+var db = new sqlite3.cached.Database('./database/tcc_log.db3');
+
 var server = express();
+server.use(bodyParser.json());
 
 
 const listUser = [
-    {nome:'Thiago Henrique de Godoy', profissao:'Mecânico'},
-    {nome:'Pedro', profissao:'Pintor'},
+    { nome: 'Thiago Henrique de Godoy', profissao: 'Mecânico' },
+    { nome: 'Pedro', profissao: 'Pintor' },
 ];
 
-server.get('/',(request,response)=>{
+server.get('/', (request, response) => {
     response.send('Servidor no Ar!!');
 });
 
-server.get('/api/getUsers',(request,response)=>{
+server.get('/api/getUsers', (request, response) => {
     response.send(listUser);
 
 });
 
-server.post('/uploads',(req,response)=>{
-     // create an incoming form object
-  var form = new formidable.IncomingForm();
 
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
+server.post('/log/save', (request, response) => {    
 
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
+    let log = request.body;
 
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  });
+    db.run(`INSERT INTO log ( interface, module, start_date, end_date, start_battery, end_battery) VALUES ('${log.interface}', '${log.module}', '${log.start_date}', '${log.end_date}', '${log.start_battery}', '${log.end_battery}')`);
 
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
+    response.sendStatus(200);
 });
 
-server.listen(9000,()=>{
+server.post('/uploads', (req, response) => {
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+});
+
+server.listen(9000, () => {
     console.log('Servidor no ar');
 
 });
- 
